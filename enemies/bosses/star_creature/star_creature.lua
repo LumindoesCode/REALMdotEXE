8aef2eaabd79391ebe76bccd876001a3bb984f5c
LumindoesCode <Roanvandekaa@gmail.com>
2025-04-27 13:16:47 +0200

    More Stuff

--- enemies/bosses/star_creature/star_creature.lua
+++ enemies/bosses/star_creature/star_creature.lua
@@ -1,5 +1,3 @@
-local easings = require("LuminsHelpers/easing")
-
 star_sprite = custom_sprite("REALMdotEXE/enemies/bosses/star_creature/star_creature.png", 1, 24, 27, 0)
 
 
@@ -53,6 +51,8 @@ function star.Draw(obj)
         local sp = spawn_particle(get_var(obj, "x"), get_var(obj, "y"), 0, 0, star_sprite)
 
         set_var(sp, "image_alpha", 0.75)
+        set_var(sp, "image_xscale", 0.75)
+        set_var(sp, "image_yscale", 0.75)
 
 
         local fader = function(v)
@@ -67,8 +67,6 @@ function star.Draw(obj)
 
         LumHelp.AddCallback(sp, fader)
     end
-
-
 end
 
 
@@ -77,7 +75,6 @@ function star.Step(obj)
     set_var(obj, "siner", get_var(obj, "siner") + 1)
 
     if (get_var(obj, "behavior") == "dead") then
-        set_var(obj, "v_speed", 0)
         set_var(obj, "state", StarStates.Dead)
         init_var(obj, "dead_timer", 0)
 
@@ -86,6 +83,10 @@ function star.Step(obj)
         end
 
         if (get_var(obj, "dead_timer") == 30) then
+            play_sound(get_asset("snd_boom"), view_x + 120)
+            add_screenshake(5)
+
+        elseif (get_var(obj, "dead_timer") == 31) then
             call_function("instance_destroy", {obj})
         end
         set_var(obj, "dead_timer", get_var(obj, "dead_timer") + 1)
@@ -104,18 +105,21 @@ function star.Step(obj)
 
         if (get_var(obj, "ai_timer") >= 60) then
 
-            set_var(obj, "vspeed", math.sin(get_var(obj, "siner") / 25) * 0.5)
+            set_var(obj, "vspeed", math.sin(get_var(obj, "siner") / 25) * 1)
 
             set_var(obj, "spin_rando", math.random(0, 350))
     
             local stop_and_schmoove = function(v)
                 init_var(v, "stop_timer", 0)
-                if (get_var(v, "stop_timer") == 25) then
+                init_var(v, "initial_x", get_var(v, "x"))
+                init_var(v, "initial_y", get_var(v, "y"))
+
+                if (get_var(v, "stop_timer") == 15) then
                     set_var(v, "hspeed", 0)
                     set_var(v, "vspeed", 0)
-                elseif (get_var(v, "stop_timer") == 35) then
+                elseif (get_var(v, "stop_timer") == 25) then
                     play_sound(get_asset("snd_pball_absorb"), get_var(v, "x"))
-                    local recent_dir = get_direction(get_var(v, "x"), get_var(v, "y"), player_x, player_y)
+                    local recent_dir = get_direction(get_var(v, "initial_x"), get_var(v, "initial_y"), player_x, player_y)
                     set_var(v, "hspeed", recent_dir.x * 2)
                     set_var(v, "vspeed", recent_dir.y * 2)
                 end
@@ -123,11 +127,29 @@ function star.Step(obj)
             end
     
             if (math.fmod(get_var(obj, "ai_timer"), 50) == 0) then
+                init_var(obj, "invert", false)
+                if (get_var(obj, "invert")) then
+                    set_var(obj, "image_angle", 15)
+                else
+                    set_var(obj, "image_angle", -15)
+                end
+                set_var(obj, "invert", not get_var(obj, "invert"))
+                print(get_var(obj, "invert"))
+
+                local star_amount = 90
+                local loop_offset = 45
+
+                if (get_global("game_loop") >= 5) then
+                    star_amount = 72
+                    loop_offset = 0
+                end
+
+
                 play_sound(get_asset("snd_revolver"), get_var(obj, "x"))
-                for i = 1, 350, 72 do
+                for i = 1, 350, star_amount do
                     local star_stop = spawn_projectile(get_var(obj, "x"), get_var(obj, "y"), rot_x(i + get_var(obj, "spin_rando")) * 1.5, rot_y(i + get_var(obj, "spin_rando")) * 1.5, get_asset("spr_enemy_bullet_dstar"))
                     LumHelp.AddCallback(star_stop, stop_and_schmoove)
-                    spawn_projectile(get_var(obj, "x"), get_var(obj, "y"), -rot_x(i + get_var(obj, "spin_rando")) * 1.5, -rot_y(i + get_var(obj, "spin_rando")) * 1.5, get_asset("spr_enemy_bullet_dstar_blue"))
+                    spawn_projectile(get_var(obj, "x"), get_var(obj, "y"), -rot_x(i + get_var(obj, "spin_rando") + loop_offset) * 1.5, -rot_y(i + get_var(obj, "spin_rando") + loop_offset) * 1.5, get_asset("spr_enemy_bullet_dstar_blue"))
                 end
                 if hard_mode then
                     for i = 1, 350, 45 do
@@ -136,10 +158,14 @@ function star.Step(obj)
                 end
             end
             
+
+            set_var(obj, "image_angle", lerp(get_var(obj, "image_angle"), 0, 0.2 * 0.2))
+            
         end
     end
 
     if (get_var(obj, "state") == StarStates.Dead) then
+        set_var(obj, "vspeed", 0)
         set_var(obj, "ai_timer", 0)
     end
     
